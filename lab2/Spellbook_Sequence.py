f2e284ab9ab7f7bcef112df4e94b556b6c3dcc2e
Mewan Dimalsha <115539818+mewanDimalsha@users.noreply.github.com>
2023-11-29 19:19:37 +0000

    initaial

---
+++ lab2/Spellbook_Sequence.py
@@ -0,0 +1,12 @@
+x, y = map(int, input().split())
+
+first_num_array = []
+second_num_array = []
+
+for i in range(x):
+    elements = input().split()
+
+for j in range(y):
+    elements = input().split()
+
+print(first_num_array)
